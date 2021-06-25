3711991562231700c505d9914768ee478850c222
FreyaLi <lxf223307@163.com>
2021-06-25 18:07:53 +0800

    mini grep

--- simpleredis/adder/target/debug/deps/adder-ef498f7c831aab4c.d
+++
@@ -1,5 +0,0 @@
-/Users/lixiaofang/project/rust-study/simpleredis/adder/target/debug/deps/adder-ef498f7c831aab4c: src/lib.rs
-
-/Users/lixiaofang/project/rust-study/simpleredis/adder/target/debug/deps/adder-ef498f7c831aab4c.d: src/lib.rs
-
-src/lib.rs:
