af0f745366946b6525c05e8af4a1b7ea7f39e92f
lljxx1 <lljxx1@gmail.com>
2021-06-27 19:34:30 +0800

    add

--- simpleredis/src/lib.rs
+++ simpleredis/src/lib.rs
@@ -1,3 +1,32 @@
 
 
 pub mod database;
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use database::Databse;
+
+    #[test]
+    fn test_set() {
+
+        let mut db = Databse::new();
+        for id in 0..1 {
+            let keyName = format!("hello{}", id);
+            println!("setKey: {}", keyName);
+            db.set(keyName, format!("value={}", id));
+        }
+
+        let key  = "hello1".to_string();
+        assert_eq!(
+            "value=1".to_string(),
+            db.get(&key).unwrap()
+        );
+
+    }
+}
--- simpleredis/src/main.rs
+++ simpleredis/src/main.rs
@@ -1,49 +1,44 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use mini_redis::cmd::Command;
 use simpleredis::database::Databse;
 
 
-fn main() {
 
+#[tokio::main]
+async fn main() {
     let mut db = Databse::new();
-    for id in 0..100 {
-        let keyName = format!("hello{}", id);
-        println!("setKey: {}", keyName);
-        db.set(keyName, format!("value={}", id));
+    let listenner = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (socket, _) = listenner.accept().await.unwrap();
+        process(socket, &db).await;
     }
-
-
-    let key  = "hello89".to_string();
-    match db.get(&key) {
-        None => println!("not found"),
-        Some(value) => {
-            println!("value: {}", value);
-        }
-    }
-
-    
-    // if let Some(value) = db.get(key) {
-    //     println!("value: {}", value);
-    // }
-    // let value = db.get("hello".to_string());
-    // println!("valueis: {}", value);
-
 }
-// #[tokio::main]
-// async fn main() {
-//     let listenner = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-//     loop {
-//         let (socket, _) = listenner.accept().await.unwrap();
-//         process(socket).await;
-//     }
-// }
 
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: &Databse) {
     let mut connection = Connection::new(socket);
 
     if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+        // println!("GOT: {:?}", frame);
+
+        // let cmdName = cmd.get_name();
+        match Command::from_frame(frame) {
+            Err(er) => println!("get comand name failed"),
+            Ok(cmd) => {
+                match cmd {
+                    Command::Publish(_) => {},
+                    Command::Set(_) => {},
+                    Command::Subscribe(_) => {},
+                    Command::Unsubscribe(_) => {},
+                    Command::Unknown(cmd) => {},
+                    Command::Get(_)=> {
+                        let key = _.key();
+                        println!("get comand {}", key);
+                    }
+                }
+            }
+        }
 
         let response = Frame::Error("unimplemented".to_string());
         connection.write_frame(&response).await.unwrap();
